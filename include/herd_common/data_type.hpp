774139fb7cf7a5393ae62a9c597443e7e96bc061
Xert <xertdev@gmail.com>
2023-01-08 15:51:01 +0100

    Data type enum pack

--- include/herd_common/data_type.hpp
+++ include/herd_common/data_type.hpp
@@ -1,23 +1,30 @@
 #ifndef HERD_COMMON_COLUMN_TYPE_HPP
 #define HERD_COMMON_COLUMN_TYPE_HPP
 
+#include <cstdint>
+
 
 namespace herd::common
 {
-	enum class DataType
+	namespace data_type
 	{
-		BIT,
+		enum class DataType: uint8_t
+		{
+			BIT,
 
-		UINT8,
-		UINT16,
-		UINT32,
-		UINT64,
+			UINT8,
+			UINT16,
+			UINT32,
+			UINT64,
 
-		INT8,
-		INT16,
-		INT32,
-		INT64
-	};
+			INT8,
+			INT16,
+			INT32,
+			INT64
+		};
+	}
+
+	using namespace data_type;
 }
 
 #endif //HERD_COMMON_COLUMN_TYPE_HPP
